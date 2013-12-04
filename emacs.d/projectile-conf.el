1b4678999c0be638d4efdf38ae2fe4015c45a9b9
René Zurita Corro <zc.rene@gmail.com>
2013-12-04 14:14:35 -0600

    update: 'projectile-conf.el' file.

--- emacs.d/projectile-conf.el
+++ emacs.d/projectile-conf.el
@@ -6,6 +6,11 @@
 ;; en un `eval-after-load'.
 ;; (setq-default projectile-keymap-prefix (kbd "C-c f"))
 
+;; El sistema default permite la interacción con `iciciles', aún hay
+;; que probar más detalladamente este comportamiento
+(setq projectile-completion-system 'default)
+
 (add-hook 'python-mode-hook 'projectile-on)
+(add-hook 'LaTeX-mode-hook 'projectile-on)
 
 (provide 'projectile-conf)
