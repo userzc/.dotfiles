b855c2a9d2d2f6ebf190f406bce9a9922e705684
René Zurita Corro <zc.rene@gmail.com>
2015-06-26 11:14:46 -0500

    update: enable projectile-mode for sh-mode.

--- emacs.d/settings-lisp/projectile-conf.el
+++ emacs.d/settings-lisp/projectile-conf.el
@@ -29,6 +29,7 @@
 (add-hook 'feature-mode-hook 'projectile-mode)
 (add-hook 'scss-mode-hook 'projectile-mode)
 (add-hook 'dired-mode-hook 'projectile-mode)
+(add-hook 'sh-mode-hook 'projectile-mode)
 (add-hook 'js2-mode-hook 'projectile-mode)
 (add-hook 'js-mode-hook 'projectile-mode)
 
