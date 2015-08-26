efeb96c174eb4e2268c1662b851eb5fe4331f0c0
René Zurita Corro <zc.rene@gmail.com>
2015-08-26 09:03:20 -0500

    update: i3 exit menu and .zshrc

--- bin/i3_exit_dmenu.sh
+++ bin/i3_exit_dmenu.sh
@@ -7,17 +7,22 @@
 # export $fn="-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*"
 
 # -nb $nb  -nf $nf  -sb $sb  -sf $sf  -fn $fn
+message="Suspend
+Hibernate [-- CAUTION --]
+Reboot
+Shutdown
+Logout"
 
 # hibernate doesn't seem to work on the Dell Inspiron
-select=$( echo -e "SUSPEND\nREBOOT\nSHUTDOWN\nLOGOUT" | dmenu -l 5 -i -p  "Exit action to perform: ")
+select=$( echo -e "$message" | dmenu -l 5 -i -p  "Exit action to perform: ")
 
 # Fuente
 # http://askubuntu.com/questions/454039/what-command-is-executed-when-shutdown-from-the-graphical-menu-in-14-04
 
 case "$select" in
-    "SUSPEND") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Suspend" boolean:true ;;
-    "HIBERNATE") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Hibernate" boolean:true ;;
-    "REBOOT") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Reboot" boolean:true ;;
-    "SHUTDOWN") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.PowerOff" boolean:true ;;
-    "LOGOUT") i3-msg exit;;
+    "Suspend") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Suspend" boolean:true ;;
+    "Hibernate [-- CAUTION --]") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Hibernate" boolean:true ;;
+    "Reboot") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Reboot" boolean:true ;;
+    "Shutdown") dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.PowerOff" boolean:true ;;
+    "Logout") i3-msg exit;;
 esac;
--- zshrc
+++ zshrc
@@ -1,5 +1,5 @@
 # Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
