39275f3139a62a7efa42ad93d280fc8320323188
René Zurita Corro <zc.rene@gmail.com>
2017-11-24 15:22:36 -0600

    update: rofi config file for i3_exit_dmenu.sh

--- bin/i3_exit_dmenu.sh
+++ bin/i3_exit_dmenu.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# export $nb="#151515"
-# export $nf="#999999"
-# export $sb="#f00060"
-# export $sf="#000000"
-# export $fn="-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*"
-
 # colors from ~/.emacs.d/elpa/monokai-theme-20130703.1811/monokai-theme.el
 # set $color_nf '#89BDFF'
 color_nf='#66D9EF'
@@ -13,12 +7,12 @@ color_nb='#49483E'
 # color_sf='#FD971F'
 color_sf='#F92672'
 color_sb='#171A0B'
-bg-color='#2f343f'
+bg_color='#2f343f'
 # # So far I can't load the apropiarte fonts
 fn='pango=:DejaVu Sans Mono 9'
+fn1="System San Francisco Display 18"
 # set $fn 'Meslo LG S for Powerline RegularForPowerline'
 
-# -nb $nb  -nf $nf  -sb $sb  -sf $sf  -fn $fn
 message="Suspend
 Hibernate [-- CAUTION --]
 Reboot
@@ -27,8 +21,10 @@ Lock
 Logout"
 
 # hibernate doesn't seem to work on the Dell Inspiron
-# select=$( echo -e "$message" | dmenu -l 5 -i -p  "Exit action to perform: ")
-select=$( echo -e "$message" | rofi -sidebar-mode -dmenu -i -p "[exit]:" -width 100 -padding 50 -hide-scrollbar -lines 5 -eh 2  -opacity "85" -bw 0 -bc "$bg-color" -bg "$color_nb" -fg "$color_nf" -hlbg "$color_sb" -hlfg "$color_sf" -hide-scroll-bar -font "System San Francisco Display 18")
+select=$( echo -e "$message" | rofi -sidebar-mode -dmenu -i -p "[exit]:" -width 100 -padding 50 -hide-scrollbar -lines 5 -eh 2  -opacity "85" -bw 0 -bc "$bg_color" -bg "$color_nb" -fg "$color_nf" -hlbg "$color_sb" -hlfg "$color_sf" -hide-scroll-bar -font "$fn")
+
+# # Ubuntu 17.10
+# select=$( echo -e "$message" | rofi -dmenu -i -p "[exit]:" -sidebar-mode -config ~/.config/rofi/exit_config -hide-scrollbar -font "$fn1")
 
 # Fuente
 # http://askubuntu.com/questions/454039/what-command-is-executed-when-shutdown-from-the-graphical-menu-in-14-04
