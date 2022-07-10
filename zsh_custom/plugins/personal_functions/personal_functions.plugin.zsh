3baf90b9e7f92097a496be72d11994b8330b1474
René Zurita Corro <zc.rene@gmail.com>
2022-07-09 23:52:40 -0500

    update: function for corne keymap reference

--- zsh_custom/plugins/personal_functions/personal_functions.plugin.zsh
+++ zsh_custom/plugins/personal_functions/personal_functions.plugin.zsh
@@ -1,4 +1,5 @@
 function list-dir-repos-url() {
+    # This function lists the urls of all subdirectories of the target directory
     if [[ $# == 1 ]]; then
 	current_directory=$(pwd)
         target_directory=$1
@@ -18,5 +19,19 @@ function list-dir-repos-url() {
     else
 	echo "usage: list-dir-repos-url directory-path"
 	echo "       Prints all the VCS urls registered on the subdirectories of directory-path, useful for reference and search"
+    fi
+}
+
+function sck() {
+    # This function shows the layer reference for the current corne keyboard in the local repo
+    if [[ $# == 1 ]]; then
+        grep -B 1 -A 10 " \[$1\] " ~/instalados-localmente/qmk_firmware/keyboards/crkbd/keymaps/userzc/keymap.c
+    else
+        echo "usage: sck layer"
+        echo "       Show Current Keymap for layer as reference for the current userzc keymap in the local repo"
+        echo "layers: 0 -- Default"
+        echo "        1 -- Lower"
+        echo "        2 -- Raise"
+        echo "        3 -- Adjust"
     fi
 }
